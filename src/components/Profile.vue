90aa61b7e728ed51a9b0b6b734685670971fc999
root <root@LAPTOP-9C9HD413.localdomain>
2021-03-28 02:47:40 +0800

    Updated Profile.vue file: Added UserInformation.vue component

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
     hello
+    <UserInformation></UserInformation>
   </div>
 </template>
+
+<script>
+import UserInformation from './UserInformation.vue'
+
+export default ({
+  data() {
+    
+  },
+  components: {
+    UserInformation
+  }
+})
+</script>
