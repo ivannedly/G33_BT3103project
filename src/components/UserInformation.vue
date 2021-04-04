8e2d0e6a6c17d7664f94751ca08b7976e35052fe
unknown <80308721+GLeyi@users.noreply.github.com>
2021-04-04 18:45:39 +0800

    Added change password functions / styles in UserInformation.vue

--- src/components/UserInformation.vue
+++ src/components/UserInformation.vue
@@ -11,22 +11,41 @@
         "Edit Card Details" Button
         -->
         <img src = "https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8dHJlZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt = "Photo of Tree"> <!--need to make sure they are all cropped to the same size-->
-        <br> <!--To be changed later-->
+        <br> <!--To be changed later with photo url from databse-->
         <button>Change Profile Photo</button>
         <p><b>NAME</b></p>
         <p class="field">{{name}}</p>
         <p><b>EMAIL</b></p>
         <p class="field">{{email}}</p>
-        <button>Change Password</button> 
+        <button id="changePasswordButton" v-on:click="openChangePasswordBox">Change Password</button> 
         <button>Update Personal Information</button>
         <br><br>
         <button>Edit Card Details</button>
+        
+        <div id="changePasswordBox">
+            <div id="changePasswordContent">
+                <p class="close" v-on:click="closeChangePasswordBox">Close this Page</p>
+                <h1><b>Change Password</b></h1>
+                <form>
+                    <label for="pwd1">New Password: </label>
+                    <input type="password" id="pwd1" name="pwd1" v-model="newPassword1">
+                    <br><br>
+                    <label for="pwd2">Confirm Your New Password: </label>
+                    <input type="password" id="pwd2" name="pwd2" v-model="newPassword2">
+                    <br><br>
+                    <p style="color: red;">{{alertMessage}}</p>
+                    <input type="submit" value="Change Password" v-on:click.prevent="changePassword">
+                </form>
+            </div>
+        </div>
 
     </div>
 </template>
 
 <script>
 import database from '../firebase.js';
+import firebase from 'firebase';
+
 
 export default ({
     data() {
@@ -39,6 +58,9 @@ export default ({
             moneySave: "",
             name: "",
             travelNum: 0,
+            newPassword1: "",
+            newPassword2: "",
+            alertMessage: "",
         }
     },
     methods: {
@@ -59,6 +81,28 @@ export default ({
                 this.travelNum = doc.data().travelNum;
                 console.log(this.name);
             })
+        },
+        openChangePasswordBox() {
+            console.log("Activating openChangePasswordBox...");
+            var modal = document.getElementById("changePasswordBox");
+            modal.style.display = "block";
+        },
+        closeChangePasswordBox() {
+            var modal = document.getElementById("changePasswordBox");
+            modal.style.display = "none";
+        },
+        // Need to try this method again after sign-in page has been added as "user" currently has null value.
+        changePassword() {
+            if (this.newPassword1 != this.newPassword2 || this.newPassword1.length == 0) {
+                console.log("Passwords do not match");
+                this.alertMessage = "The password(s) you have entered do not match. Please try again.";
+            } else {
+                console.log("Passwords match");
+                this.alertMessage = "";
+                var user = firebase.auth().currentUser;
+                user.updatePassword(this.newPassword1);
+                alert("Your password has been updated successfully");
+            }
         }
     },
     created() {
@@ -84,5 +128,44 @@ button {
     width: 300px;
     border-radius: 15px;
 }
+
+#changePasswordBox {
+    display: none;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgb(0,0,0);
+    background-color: rgba(0,0,0,0.4);
+}
+
+#changePasswordContent {
+    background-color: white;
+    margin: 15% auto;
+    padding: 20px;
+    border: 1px solid black;
+    width: 80%;
+}
+
+.close {
+    color: black;
+    float: right;
+    font-size: 15px;
+    text-decoration: underline;
+}
+
+.close:hover,
+.close:focus {
+    color: red;
+    cursor: pointer;
+}
+
+form {
+    text-align: left;
+}
+
 </style> 
 -->
