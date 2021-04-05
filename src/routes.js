75c53aee8deffec537a86d37dad262cd5377712b
SIQI-47 <e0376980@u.nus.edu>
2021-04-06 00:59:19 +0800

    update signin signup pages

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,16 @@ import Profile from './components/Profile.vue'
 import Plan from './components/Plan.vue'
 import Plantpal from './components/Plantpal.vue'
 import Howtouse from './components/Howtouse.vue'
+import SignIn from './components/SignIn.vue'
+import SignUp from './components/SignUp.vue'
 
 export default [
   { path: '/', component: Home },
   { path: '/profile', component: Profile },
   { path: '/plan', name: "modify", component: Plan },
   { path: '/plantpal', component: Plantpal },
-  { path: '/howtouse', component: Howtouse }
+  { path: '/howtouse', component: Howtouse },
+  { path: '/login', component: SignIn},
+  { path: '/signup', component: SignUp}
 ]
 
