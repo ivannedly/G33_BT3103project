b239a58a9312091802e0b92f374b3de0d857685a
Ivan Foo <ivanfoo@Ivans-MacBook-Pro.local>
2021-04-13 12:00:29 +0800

    added PlantProgress.vue

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,14 +2,12 @@
   <div>
     <UserInformation id="userInformation"></UserInformation>
     <UserStatistics id="userStatistics"></UserStatistics>
-    <GoogleMap/> 
   </div>
 </template>
 
 <script>
 import UserInformation from './UserInformation.vue'
 import UserStatistics from './UserStatistics.vue'
-import GoogleMap from './GoogleMap.vue'
 
 export default ({
   data() {
@@ -18,12 +16,10 @@ export default ({
   components: {
     UserInformation,
     UserStatistics,
-    GoogleMap
   }
 })
 </script>
 
-<!--
 <style scoped>
 
 div {
@@ -43,4 +39,4 @@ div {
 }
 
 </style>
--->
+
--- src/main.js
+++ src/main.js
@@ -3,15 +3,17 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 import App from './App.vue'
 import Routes from './routes.js'
 import VueRouter from 'vue-router'
+import VueEllipseProgress from 'vue-ellipse-progress';
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(VueGoogleMaps, {
   load: {
-    key: '',
+    key: 'AIzaSyAfxu1U2TjLFftT4Au8Ybdzj3cNun28lZk',
     libraries: 'places',
   }
 });
+Vue.use(VueEllipseProgress)
 
 const myRouter = new VueRouter({
   routes: Routes,
