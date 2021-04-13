e0fcf9d33903004978f32063b831a29fcb8a8473
Ivan Foo <ivanfoo@Ivans-MacBook-Pro.local>
2021-04-13 16:19:28 +0800

    updated plantpal page and profile page

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyDFPwQJ9jszMkeMVZQ78MzMvFPgisbCS7Q',
+    key: '',
     libraries: 'places',
   }
 });
