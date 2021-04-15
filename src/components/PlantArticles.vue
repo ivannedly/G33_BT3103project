5ab49d8d3dd2b71cee6073a66d6c22cbf5660949
elizabeth <elizabethlau07@gmail.com>
2021-04-15 18:38:44 +0800

    update PlantPal page

---
+++ src/components/PlantArticles.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class = "articles">
+        
+
+    </div>
+</template>
+
+<script>
+
+</script>
--- src/components/Plantpal.vue
+++ src/components/Plantpal.vue
@@ -1,16 +1,63 @@
 <template>
-  <PlantProgress/>
+<div class="row">
+  
+  <div class="userProfile">
+    <div class="welcome-msg">Welcome Back {{this.name}}!</div>
+    <UserInfo></Userinfo>
+    <p class="message"><router-link to="/profile" exact>More in Profile</router-link></p>
+  </div>
+  <div class="plantInfo">
+    <div class="plant-info">You are currently at: </div>
+    <PlantProgress></PlantProgress>
+  </div>
+</div>
 </template>
 
 <script>
-import PlantProgress from "./PlantProgress.vue"
 
-export default {
+import PlantProgress from './PlantProgress.vue';
+
+
+export default { 
   components: {
-    PlantProgress
-  }
+    PlantProgress,
+    //UserInfo,
+  },
+  
 }
 </script>
-
 <style scoped>
+
+.userProfile {
+  float: left;
+  width: 32%;
+  background-color:moccasin;
+  border-radius: 10px;
+  padding-left: 10px;
+}
+.plantInfo {
+  float: right;
+  width: 29%;
+  background-color:moccasin;
+  border-radius: 10px;
+  height: 600px;
+  padding: 5px;
+}
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.welcome-msg {
+  padding-top: 50px;
+  text-align: center;
+  padding-bottom: 30px;
+}
+.plant-info {
+  text-align: left;
+  padding-top: 50px;
+  padding-left: 20px;
+  padding-bottom: 30px;
+}
 </style>
