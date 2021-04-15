5c4ed03235bda4b1bfa21d58b0cb34b631f66c05
unknown <80308721+GLeyi@users.noreply.github.com>
2021-04-16 03:54:44 +0800

    StatisticsGraph.vue: Removed comments

--- src/components/StatisticsGraph.vue
+++ src/components/StatisticsGraph.vue
@@ -14,16 +14,12 @@ export default({
     props: {
         cumulativeCarbonCut: {},
         journeyDate: {},
-        //graphData: {}
     },
     data(){
         return {
             data:[{
-                //x: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
                 x: this.journeyDate,
-                //y: [10,15,13,17],
                 y: this.cumulativeCarbonCut,
-                //y: this.graphData[0],
                 type:"scatter"
             }],
             layout:{
