fa186f3dc72052dd361950f8085bd40528430a8d
unknown <80308721+GLeyi@users.noreply.github.com>
2021-04-17 04:48:32 +0800

    UserStatistics.vue: Updates

--- src/components/UserStatistics.vue
+++ src/components/UserStatistics.vue
@@ -8,12 +8,17 @@
         Money Saved (from rewards)
         -->
         <h1>JOURNEY STATISTICS</h1>
-        <p><b>TOTAL NUMBER OF JOUNEYS: </b>{{travelNum}}</p>
+        <p><b>TOTAL NUMBER OF JOURNEYS: </b>{{totalNoOfJourneys}}</p>
         <p><b>TOTAL DISTANCE TRAVELLED (KM): </b>{{totalDistance}}</p>
         <p><b>TOTAL CARBON FOOTPRINT REDUCED (KG): </b>{{totalCarbonCut}}</p>
         <p><b>TOTAL MONEY SAVED (IN SGD): </b>{{moneySave}}</p>
+        <br>
+        <button type="button" v-on:click="showCarbonCutAgainstTimeGraph()"> Carbon Cut VS Time </button> 
+        <button type="button" v-on:click="showDistanceAgainstTimeGraph()"> Distance VS Time </button>
+        <button type="button" v-on:click="showNoOfJourneysAgainstTimeGraph()"> Number of Journeys VS Time </button>
         <CarbonCutAgainstTimeGraph v-bind:cumulativeCarbonCut="cumulativeCarbonCut" v-bind:journeyDate="journeyDate"></CarbonCutAgainstTimeGraph>
         <DistanceAgainstTimeGraph v-bind:cumulativeDistance="cumulativeDistance" v-bind:journeyDate="journeyDate"></DistanceAgainstTimeGraph>
+        <NoOfJourneysAgainstTimeGraph v-bind:cumulativeDistance="cumulativeNoOfJourneys" v-bind:journeyDate="journeyDate"></NoOfJourneysAgainstTimeGraph>    
     </div>
 </template>
 
@@ -21,27 +26,30 @@
 import database from '../firebase.js';
 import CarbonCutAgainstTimeGraph from './CarbonCutAgainstTimeGraph.vue';
 import DistanceAgainstTimeGraph from './DistanceAgainstTimeGraph.vue';
+import NoOfJourneysAgainstTimeGraph from './NoOfJourneysAgainstTimeGraph.vue';
 
 export default ({
     data() {
         return {
+            totalNoOfJourneys: 0,
             totalCarbonCut: 0,
             totalDistance: 0,
             moneySave: 0,
-            travelNum: 0,
             journeyCarbonCut: [],
             journeyDate: [],
+            cumulativeNoOfJourneys: [],
             cumulativeCarbonCut: [],
             cumulativeDistance: [],
         }
     },
     components: {
         CarbonCutAgainstTimeGraph,
-        DistanceAgainstTimeGraph
+        DistanceAgainstTimeGraph,
+        NoOfJourneysAgainstTimeGraph
     },
     methods: {
         fetchUserData() {
-            database.collection('users').doc(localStorage.uid).get().then(doc => {
+            database.collection('users').doc(localStorage.uid).get().then(doc => {               
                 // Get distance
                 var currentTotalDistance = 0;
                 for (var k = 0; k < doc.data().distance.length; k++) {
@@ -59,15 +67,16 @@ export default ({
                 this.totalCarbonCut = currentTotalCarbonCut;
 
                 // Get journey dates
-                var journeyDateFromData = doc.data().journeyDate;
-                for (var j = 0; j < journeyDateFromData.length; j++) {
-                    var currentJourneyDate = journeyDateFromData[j];
+                var currentTotalNoOfJourneys = 0;
+                for (var j = 0; j < doc.data().journeyDate.length; j++) {
+                    currentTotalNoOfJourneys += 1;
+                    this.cumulativeNoOfJourneys[j] = currentTotalNoOfJourneys;
+                    var currentJourneyDate = doc.data().journeyDate[j];
                     this.journeyDate.push(currentJourneyDate.toDate().toISOString());
                 }
+                this.totalNoOfJourneys = currentTotalNoOfJourneys;
 
-                this.moneySave = doc.data().moneySave;
-                this.travelNum = doc.data().travelNum;
-            
+                this.moneySave = doc.data().moneySave;            
             })
         },
     },
@@ -88,5 +97,21 @@ h1 {
     color: black;
     background-color: white;
     padding: 10px;
+    border-radius: 10px;
+}
+
+button {
+  text-align: center;
+  background: none;
+  transition-duration: 0.4s;
+  padding: 10px 24px;
+  background-color:black;
+  color: white;
+  width: 33%;
+}
+
+button:hover{
+  background-color:white;
+  color: black;
 }
 </style>
