f2d016415307a70e85365deceb468ad7f96efb60
unknown <80308721+GLeyi@users.noreply.github.com>
2021-04-18 17:22:27 +0800

    SignUp.vue : Updates styling

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -3,23 +3,26 @@
     <h2>REGISTER</h2>
     <div class="form">
       <form class = "signup-form">
+        <br><h3>PERSONAL INFORMATION</h3>
         <div>Email:</div>
         <input type="text" v-model="email" placeholder="Email"/>
         <div>Username:</div>
         <input type="text" v-model="name" placeholder="Name"/>
         <div>Mobile Number:</div>
-        <input type="tel" v-model="phone" placeholder="Mobile Number"/>
+        <input type="tel" v-model="phone" placeholder="E.g. 9123 4567"/>
         <div>Password:</div>
         <input type="password" v-model="password" placeholder="Password"/>
+        <br><br><h3>CARD DETAILS</h3>
         <div>Cardholder Name:</div>
         <input type="text" v-model="cardholder" placeholder="Cardholder Name"/>
         <div>Credit Card Number:</div>
-        <input type="string" v-model="creditNum" placeholder="Credit Card Number"/>
+        <input type="string" v-model="creditNum" placeholder="E.g. 1234-1234-1234-1234"/>
         <div>CVV:</div>
-        <input type="string" v-model="cvv" placeholder="CSV"/>
+        <input type="string" v-model="cvv" placeholder="E.g. 333"/>
         <div>Card Expiry date:</div>
         <input type="date" v-model="expiry" placeholder="Expiry Date"/><br><br>
-        <p style="color: red;">{{alertMessage}}</p>
+        <p style="color: red;">{{invalidCardMessage}}</p>
+        <br><p style="color: red;">{{alertMessage}}</p>
         <button v-on:click.prevent = "signUp">Sign Up</button>
         <p class="message">Already have an account? Click <router-link to="/login" exact>here</router-link> to log in.</p>
       </form>
