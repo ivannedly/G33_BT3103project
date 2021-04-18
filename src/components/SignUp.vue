216d155a16e67b914d500740c92d34250ee2ac60
unknown <80308721+GLeyi@users.noreply.github.com>
2021-04-18 18:21:22 +0800

    SignUp.vue: Added alert for successful signup

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -4,25 +4,26 @@
     <div class="form">
       <form class = "signup-form">
         <br><h3>PERSONAL INFORMATION</h3>
-        <div>Email:</div>
+        <div>Email*:</div>
         <input type="text" v-model="email" placeholder="Email"/>
-        <div>Username:</div>
+        <div>Username*:</div>
         <input type="text" v-model="name" placeholder="Name"/>
         <div>Mobile Number:</div>
         <input type="tel" v-model="phone" placeholder="E.g. 9123 4567"/>
-        <div>Password:</div>
+        <div>Password*:</div>
         <input type="password" v-model="password" placeholder="Password"/>
         <br><br><h3>CARD DETAILS</h3>
-        <div>Cardholder Name:</div>
+        <div>Cardholder Name*:</div>
         <input type="text" v-model="cardholder" placeholder="Cardholder Name"/>
-        <div>Credit Card Number:</div>
+        <div>Credit Card Number*:</div>
         <input type="string" v-model="creditNum" placeholder="E.g. 1234-1234-1234-1234"/>
-        <div>CVV:</div>
+        <div>CVV*:</div>
         <input type="string" v-model="cvv" placeholder="E.g. 333"/>
-        <div>Card Expiry date:</div>
+        <div>Card Expiry date*:</div>
         <input type="date" v-model="expiry" placeholder="Expiry Date"/><br>
         <p style="color: red;">{{alertMessage}}</p>
         <button v-on:click.prevent = "signUp">Sign Up</button>
+        <br><p>* indicates required fields.</p><br>
         <p class="message">Already have an account? Click <router-link to="/login" exact>here</router-link> to log in.</p>
       </form>
     </div>
@@ -51,7 +52,9 @@ export default {
     signUp: function(){
       var currentTime = new Date();
       var enteredExpiryDate = new Date(this.expiry);
-      if (enteredExpiryDate.getTime() <= currentTime.getTime() || this.creditNum.length != 19 || this.cvv.length != 3) {
+      if (this.email.length == 0 || this.name.length == 0 || this.password.length == 0 || this.cardholder.length==0 || this.creditNum.length==0 || this.cvv.length==0 || this.expiry==0) {
+        this.alertMessage = "You have not filled in one or more of the required fields. Please try again.";
+      } else if (enteredExpiryDate.getTime() <= currentTime.getTime() || this.creditNum.length != 19 || this.cvv.length != 3) {
         this.alertMessage = "The card detail(s) that you have provided is/are invalid.";
       } else {
         const new_user = {
@@ -74,6 +77,7 @@ export default {
                   .doc(cred.user.uid)
                   .set(Object.assign({}, new_user));
         }).then(() => {
+          alert("You have successfully created a new account!");
           this.$router.push({path: "/login"});
         }).catch((error) => {
           this.alertMessage = error.message;
